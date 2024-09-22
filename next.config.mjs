81eb99922daae0cce1240637cc0f8817ce704d4d
thomas maynard <t.maynard4897@gmail.com>
2024-09-22 19:46:55 -0400

    Updated next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/my-next-portfolio', // Set this to your repository name
+};
 
 export default nextConfig;
